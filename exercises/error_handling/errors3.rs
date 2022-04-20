bf103721406ec8ab488f818a8f76d4cfd09e1fef
Wesley Williams <40679192+Moto42@users.noreply.github.com>
2022-04-20 18:26:08 -0500

    passing

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -12,19 +12,14 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input);
+    let cost = total_cost(pretend_user_input).unwrap();
 
-    match cost {
-        Result::Ok(tc) => {
-            if tc > tokens {
-                println!("You can't afford that many!");
-            } else {
-                tokens -= tc;
-                println!("You now have {} tokens.", tokens);
-            };
-        },
-        Result::Err(err) => panic!("That wasn't a number"),
-    }
+    if cost > tokens {
+        println!("You can't afford that many!");
+    } else {
+        tokens -= cost;
+        println!("You now have {} tokens.", tokens);
+    };
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
